d0c69b36940e1fe336a7286826f5bbea78809569
AAShakirov <arche.sh@yandex.ru>
2024-03-06 16:20:15 +0300

    List of news is working!

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Alert, Text, View, Image } from 'react-native';
+import { Post } from './components/Post';
+
+
 
 export default function App() {
+  const [items, setItems] = React.useState();
+
+  React.useEffect(() => {
+    axios
+    .get('https://65e841a54bb72f0a9c4ec158.mockapi.io/news')
+    .then(({ data }) => {
+      setItems(data);
+    })
+    .catch(err => {
+      console.error(err);
+      Alert.alert('Custom title', 'Error in React.useEffect');
+    })
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {items.map((obj) => (
+      <Post 
+        // key={obj.id}
+        title={obj.title}
+        // text={obj.text}
+        createdAt={obj.id} 
+        />
+      ))}
+      {/* <StatusBar theme='auto' /> */}
     </View>
   );
 }
@@ -17,4 +45,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  testText: {
+    color: 'red',
+    fontSize: 24
+  }
 });
---
+++ components/Post.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+
+
+const PostView = styled.View`
+  flex-direction: row;
+  padding: 15px;
+  border-bottom-width: 5px;
+  border-bottom-color: rgba(0, 255, 0, 0.15);
+  border-bottom-style: solid;
+`;
+
+const PostDetails = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const PostImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 12px;
+  margin-right: 12px;
+`;
+
+const PostTitle = styled.Text`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const PostDate = styled.Text`
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 2px;
+`;
+
+export const Post = ({ title, createdAt}) => {
+    return <PostView>
+    {/* <PostImage source={{ uri: imageUrl }} /> */}
+    <PostDetails>
+    <PostTitle> {title} </PostTitle>
+    <PostDate> {createdAt} </PostDate>
+    </PostDetails>
+    </PostView>
+}
