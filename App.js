1e7329202409a3ac700b5f1ffd3685e1966144f0
AAShakirov <arche.sh@yandex.ru>
2024-03-11 16:23:24 +0300

    add: touchable butttons and refresh list

--- App.js
+++ App.js
@@ -3,46 +3,61 @@ import axios from 'axios';
 
 import { StatusBar } from 'expo-status-bar';
 
-import { StyleSheet, Alert, Text, View, Image, FlatList } from 'react-native';
+import { StyleSheet, Alert, Text, View, Image, FlatList, ActivityIndicator, RefreshControl, TouchableOpacity } from 'react-native';
 import { Post } from './components/Post';
 
 
 
 export default function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
   const [items, setItems] = React.useState();
-  console.log(0)
 
-  React.useEffect(() => {
+
+  const fetchPosts = () => {
+    setIsLoading(true);
     axios
     .get('https://65e841a54bb72f0a9c4ec158.mockapi.io/news')
-    // .get('https://1xstavka.ru/LiveFeed/Get1x2_VZip')
     .then(( data ) => {
-      console.log(data.data)
+      // console.log(data.data)
       setItems(data.data);
     })
     .catch(err => {
-      console.log(2)
       console.error(err);
       Alert.alert('Custom title', 'Error in React.useEffect');
     })
-  }, []);
+    .finally(() => {
+      setIsLoading(false);
+    });
+  }
+
+  React.useEffect(fetchPosts, []);
+
+  if (isLoading) {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+        }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text style={{ marginTop: 15 }}>Loading...</Text>
+      </View>
+    );
+  }
 
   console.log(3)
   return (
     <View>
       <FlatList
+        refreshControl={<RefreshControl refreshing={isLoading} onRefresh={fetchPosts}/>}
         data={items}
-        renderItem={({ item }) => (<Post title={item.title} createdAt={item.createdAt} />
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => alert('Touched')}>
+            <Post title={item.title} createdAt={item.createdAt} />
+          </TouchableOpacity>
+        
       )}
       />
-      {/* {items.map((obj) => (
-      <Post
-        title={obj.title}
-        createdAt={obj.createdAt}
-        // title={'text'}
-        // createdAt={'createdAt'}
-      />
-      ))} */}
       <StatusBar theme='auto' />
     </View>
   );
